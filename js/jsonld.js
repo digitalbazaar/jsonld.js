4ec16b8e8d49d7a47b6a18884b8c17c331dc0815
Dave Longley <dlongley@digitalbazaar.com>
2011-11-28 23:16:25 -0500

    Removed unused rdf namespace.

--- js/jsonld.js
+++ js/jsonld.js
@@ -44,7 +44,6 @@ else if(typeof(module) !== 'undefined' && module.exports)
  */
 var ns =
 {
-   rdf: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
    xsd: 'http://www.w3.org/2001/XMLSchema#'
 };
 
