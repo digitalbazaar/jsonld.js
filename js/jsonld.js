ccb3a0c103c4dd85936f9c4d1ed103564dcf22ca
Josh Mandel <joshua.mandel@childrens.harvard.edu>
2011-11-28 21:05:26 -0500

    Handle replacement of deeply-nested embeds (recursively)

--- js/jsonld.js
+++ js/jsonld.js
@@ -2776,15 +2776,18 @@ var _subframe = function(
             embed.parent[embed.key] = value['@subject'];
          }
 
-	 // Clear out any old embeds that relied on this one.
-         // (See: JSON-LD/frame 0014/Replace existing embed on 2nd pass)
-	 // TODO:  consider indexing by parent IRI for performance...
-         for(e in embeds) {
-	     if (embeds[e]['parent'] && embeds[e]['parent']['@subject']['@iri'] === iri)
-                 delete embeds[e];
-	 }
+	  // Recursively clear out old embeds that relied on this one.
+          // (See: JSON-LD/frame 0014/Replace existing embed on 2nd pass)
+	  var clear_dependents = function(iri) {
+	      for(e in embeds) {
+		  if (embeds[e]['parent'] && embeds[e]['parent']['@subject']['@iri'] === iri) {
+                      delete embeds[e];
+		      clear_dependents(e)
+		  }
+	      }
+	  };
+	  clear_dependents(iri);
       }
-      
       // update embed entry
       embed.autoembed = autoembed;
       embed.parent = parent;
