cd081e14924e30271fa00344f6623e84718923d9
Dave Longley <dlongley@digitalbazaar.com>
2012-04-21 15:34:12 -0400

    Process properties in order during framing.

--- js/jsonld.js
+++ js/jsonld.js
@@ -2124,7 +2124,10 @@ function _frame(state, subjects, frame, parent, property) {
 
       // iterate over subject properties
       var subject = matches[id];
-      for(var prop in subject) {
+      var props = Object.keys(subject).sort();
+      for(var i in props) {
+        var prop = props[i];
+
         // copy keywords to output
         if(_isKeyword(prop)) {
           output[prop] = _clone(subject[prop]);
@@ -2183,7 +2186,10 @@ function _frame(state, subjects, frame, parent, property) {
       }
 
       // handle defaults
-      for(var prop in frame) {
+      var props = Object.keys(frame).sort();
+      for(var i in props) {
+        var prop = props[i];
+
         // skip keywords
         if(_isKeyword(prop)) {
           continue;
