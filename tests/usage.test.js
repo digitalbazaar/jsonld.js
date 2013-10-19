5c3d41a75710222e44c5ecaf1452b7a30c130108
Nicholas Bollweg (Nick) <nick.bollweg@gmail.com>
2013-10-19 19:02:21 -0400

    using patterns for tests

--- tests/usage.test.js
+++ tests/usage.test.js
@@ -39,72 +39,71 @@
         "http://schema.org/name": [{"@value": "Manu Sporny"}],
         "http://schema.org/url": [{"@id": "http://manu.sporny.org/"}]
       }]
+    },
+    cb = {
+      error: function(done){
+        return function(err){ should.exist(err); done(); };
+      },
+      no_error: function(done){
+        return function(err){ should.not.exist(err); done(); };
+      },
+      deep: function(expected, done){
+        return function(err, actual){
+          assert.deepEqual(actual, expected);
+          done();
+        };
+      }
     };
-
-  function callback_error(done){
-    return function(err){ should.exist(err); done(); };
-  }
 
   module.exports = function(jsonld){
     describe("API", function(){
 
       describe("jsonld.compact", function(){
         it("should FAIL on 1 parameter", function(done){
-          jsonld.compact(callback_error(done));
+          jsonld.compact(cb.error(done));
         });
 
         it("should FAIL on null context", function(done){
-          jsonld.compact(doc, null, callback_error(done));
+          jsonld.compact(doc, null, cb.error(done));
         });
 
         it("should WIN with `null` input", function(done){
-          jsonld.compact(null, ctx, function(err){
-            should.not.exist(err);
-            done();
-          });
+          jsonld.compact(null, ctx, cb.no_error(done));
         });
 
         it("should WIN without options", function(done){
-          jsonld.compact(doc, ctx, function(err, compacted){
-            assert.deepEqual(compacted, output.compacted);
-            done();
-          });
+          jsonld.compact(doc, ctx, cb.deep(output.compacted, done));
         });
 
         it("should WIN with degenerate options", function(done){
-          jsonld.compact(doc, ctx, {}, function(err, compacted) {
-            assert.deepEqual(compacted, output.compacted);
-            done();
-          });
+          jsonld.compact(doc, ctx, {}, cb.deep(output.compacted, done));
         });
       });
 
       describe("jsonld.expand", function(){
         it("should FAIL on 1 parameter", function(done){
-          jsonld.expand(callback_error(done));
+          jsonld.expand(cb.error(done));
         });
-        
 
         it("should WIN without options", function(done){
-          jsonld.expand(doc, function(err, expanded){
-            assert.deepEqual(expanded, output.expanded);
-            done();
-          });
+          jsonld.expand(doc, cb.deep(output.expanded, done));
         });
-        
+
         it("should WIN with degenerate options", function(done){
-          jsonld.expand(doc, {}, function(err, expanded){
-            assert.deepEqual(expanded, output.expanded);
-            done();
-          });
+          jsonld.expand(doc, {}, cb.deep(output.expanded, done));
         });
       });
 
       describe("jsonld.flatten", function(){});
+
       describe("jsonld.frame", function(){});
+
       describe("jsonld.normalize", function(){});
+
       describe("jsonld.toRDF", function(){});
+
       describe("jsonld.registerRDFParser", function(){});
-    });
-  };
+
+    }); // describe("API")
+  }; // module.exports
 }).call(this);
