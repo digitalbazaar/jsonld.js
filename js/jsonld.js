491b47e1eaba2c385e0ef28955e2639531cfd374
Anders Riutta <git@andersriutta.com>
2016-03-01 11:21:11 -0800

    Undo modularizing of _removeDotSegments

--- js/jsonld.js
+++ js/jsonld.js
@@ -5988,7 +5988,44 @@ function _expandIri(activeCtx, value, relativeTo, localCtx, defined) {
   return rval;
 }
 
-var _removeDotSegments = require('remove-dot-segments');
+/**
+ * Removes dot segments from a URL path.
+ *
+ * @param path the path to remove dot segments from.
+ * @param hasAuthority true if the URL has an authority, false if not.
+ */
+function _removeDotSegments(path, hasAuthority) {
+  var rval = '';
+
+  if(path.indexOf('/') === 0) {
+    rval = '/';
+  }
+
+  // RFC 3986 5.2.4 (reworked)
+  var input = path.split('/');
+  var output = [];
+  while(input.length > 0) {
+    if(input[0] === '.' || (input[0] === '' && input.length > 1)) {
+      input.shift();
+      continue;
+    }
+    if(input[0] === '..') {
+      input.shift();
+      if(hasAuthority ||
+        (output.length > 0 && output[output.length - 1] !== '..')) {
+        output.pop();
+      } else {
+        // leading relative URL '..'
+        output.push('..');
+      }
+      continue;
+    }
+    output.push(input.shift());
+  }
+
+  return rval + output.join('/');
+}
+
 function _prependBase(base, iri) {
   // skip IRI processing
   if(base === null) {
