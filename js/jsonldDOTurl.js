e37df44665c56f7bce9599c46244d2c00d8cb685
Anders Riutta <git@andersriutta.com>
2016-03-04 21:00:19 -0800

    Remove simple option from jsonld.url.parse (again)

--- js/jsonldDOTurl.js
+++ js/jsonldDOTurl.js
@@ -2,15 +2,25 @@ import {_removeDotSegments} from './_removeDotSegments';
 export const jsonldDOTurl = {};
 
 jsonldDOTurl.parsers = {
-  simple: {
-    // RFC 3986 basic parts
-    keys: ['href','scheme','authority','path','query','fragment'],
-    regex: /^(?:([^:\/?#]+):)?(?:\/\/([^\/?#]*))?([^?#]*)(?:\?([^#]*))?(?:#(.*))?/
-  },
-  full: {
-    keys: ['href','protocol','scheme','authority','auth','user','password','hostname','port','path','directory','file','query','fragment'],
-    regex: /^(([^:\/?#]+):)?(?:\/\/((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?))?(?:(((?:[^?#\/]*\/)*)([^?#]*))(?:\?([^#]*))?(?:#(.*))?)/
-  }
+    full: {
+        keys: [
+            'href',
+            'protocol',
+            'scheme',
+            'authority',
+            'auth',
+            'user',
+            'password',
+            'hostname',
+            'port',
+            'path',
+            'directory',
+            'file',
+            'query',
+            'fragment'
+        ],
+        regex: /^(([^:\/?#]+):)?(?:\/\/((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?))?(?:(((?:[^?#\/]*\/)*)([^?#]*))(?:\?([^#]*))?(?:#(.*))?)/
+    }
 };
 
 
