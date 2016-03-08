feb3c2072e5a73967e1c0b718df60c18fed8aaac
Anders Riutta <git@andersriutta.com>
2016-03-07 22:45:46 -0800

    Cleaned up exporting, e.g., compact instead of jsonldDOTcompact

--- rollup.config.browser.js
+++ rollup.config.browser.js
@@ -18,6 +18,19 @@ config.outro = [
   fs.readFileSync(path.join('./lib/outro.browser.js'))
 ].join('\n');
 
+/*
+config.external = [
+  'superagent-cache',
+  'cache-service-cache-module',
+  'xmldom'
+];
+
+config.globals = {
+  'superagent-cache': 'superagentCache',
+  'cache-service-cache-module': 'Cache'
+};
+//*/
+
 config.footer = [
   config.footer || '',
   'window.jsonldjs = window.jsonld;',
@@ -28,20 +41,44 @@ config.plugins = [
   includePaths({
     include: {
       './NormalizeHashDOT_init.js': './lib/NormalizeHashDOT_init.browser.js',
+      'superagent-cache': './lib/placeholder.js',
+      'cache-service-cache-module': './lib/placeholder.js',
     },
     paths: ['lib'],
-    external: [],
+/*
+    external: [
+      'superagent-cache',
+      'cache-service-cache-module',
+      'xmldom'
+    ],
+//*/
     extensions: ['.js', '.json', '.html']
   }),
   nodeResolve({
     jsnext: true,
     main: true,
-    browser: true
+    browser: true,
+/*
+    skip: [
+      './node_modules/cache-service-cache-module/**',
+      './node_modules/superagent-cache/**',
+      'superagent-cache',
+      'cache-service-cache-module',
+      'xmldom'
+    ]
+//*/
   }),
   commonjs({
     include: [
       'node_modules/**'
+    ],
+/*
+    exclude: [
+      './node_modules/cache-service-cache-module/**',
+      './node_modules/superagent-cache/**',
+      './node_modules/xmldom/**',
     ]
+//*/
   }),
   uglify(),
   babel({
