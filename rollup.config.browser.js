e45b6338487a1fd37948903aef2f58c4c1d4a997
Anders Riutta <git@andersriutta.com>
2016-03-09 15:50:04 -0800

    Ignore Promise for browser build.

--- rollup.config.browser.js
+++ rollup.config.browser.js
@@ -34,6 +34,7 @@ config.plugins = [
       './NormalizeHashDOT_init.js': './lib/NormalizeHashDOT_init.browser.js',
       'cache-service-cache-module': './lib/placeholder.js',
       'es6-promise': './lib/placeholder.js',
+      'Promise': './lib/placeholder.js',
       'superagent-cache': './lib/placeholder.js',
     },
     paths: ['lib', 'node_modules', '../node_modules'],
