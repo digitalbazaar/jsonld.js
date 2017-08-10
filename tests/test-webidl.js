151222ff9cc3779be05bc86adb11042216fae835
David I. Lehn <dlehn@digitalbazaar.com>
2017-08-10 18:37:10 -0400

    Fix WebIDL test runner.
    - Use internal suite structure to properly nest mocha tests.
    - Use testharness setup() and done() so all tests run. Without this only
      one test result will be reported.

--- tests/test-webidl.js
+++ tests/test-webidl.js
@@ -17,61 +17,67 @@ module.exports = options => {
 'use strict';
 
 return new Promise((resolve, reject) => {
-  console.log('IDL0');
-  describe('Web IDL', function() {
-    console.log('IDL');
-    //it('foo', function() {});
-    add_start_callback(() => {
-      //console.log('IDL START');
-    });
-    add_test_state_callback((test) => {
-      //console.log('IDL TEST STATE', test);
-    });
-    add_result_callback(function(test) {
-      console.log('IDL RESULT', test);
-      it(test.name, function() {
-        console.log('IDL ARC IT', test.name);
-        // HACK: phantomJS can't set prototype to non-writable?
-        var msg = test.message || '';
-        if(msg.indexOf(
-          'JsonLdProcessor.prototype is writable expected false') !== -1) {
-          test.status = 0;
-        }
-        // HACK: phantomJS can't set window property to non-enumerable?
-        if(msg.indexOf(
-          '"JsonLdProcessor" is enumerable expected false') !== -1) {
-          test.status = 0;
-        }
-        //earl.addAssertion({'@id': ?}, test.status === 0);
-        assert.equal(test.status, 0, test.message);
-      });
-    });
-    add_completion_callback(function(tests, status) {
-      console.log('IDL COMPLETE', tests, status);
-      resolve();
-    });
+  // add mocha suite
+  const suite = {
+    title: 'WebIDL',
+    tests: [],
+    suites: [],
+    imports: []
+  };
 
-    // ensure that stringification tests are passed
-    var toString = Object.prototype.toString;
-    Object.prototype.toString = function() {
-      if(this === window.JsonLdProcessor.prototype) {
-        return '[object JsonLdProcessorPrototype]';
-      } else if(this && this.constructor === window.JsonLdProcessor) {
-        return '[object JsonLdProcessor]';
-      }
-      return toString.apply(this, arguments);
+  //add_start_callback(() => {});
+  //add_test_state_callback((test) => {});
+  add_result_callback(function(test) {
+    var _test = {
+      title: test.name,
+      f: null
     };
+    suite.tests.push(_test);
 
-    window.processor = new JsonLdProcessor();
+    _test.f = function(done) {
+      // HACK: phantomJS can't set prototype to non-writable?
+      var msg = test.message || '';
+      if(msg.indexOf(
+        'JsonLdProcessor.prototype is writable expected false') !== -1) {
+        test.status = 0;
+      }
+      // HACK: phantomJS can't set window property to non-enumerable?
+      if(msg.indexOf(
+        '"JsonLdProcessor" is enumerable expected false') !== -1) {
+        test.status = 0;
+      }
+      //earl.addAssertion({'@id': ?}, test.status === 0);
+      assert.equal(test.status, 0, test.message);
+      done();
+    };
+  });
+  add_completion_callback(function(tests, status) {
+    resolve(suite);
+  });
 
-    options.readFile('./tests/webidl/JsonLdProcessor.idl').then(idl => {
-      var idl_array = new IdlArray();
-      idl_array.add_idls(idl);
-      idl_array.add_objects({JsonLdProcessor: ['window.processor']});
-      idl_array.test();
-    }).catch(err => {
-      reject(err);
-    });;
+  // ensure that stringification tests are passed
+  var toString = Object.prototype.toString;
+  Object.prototype.toString = function() {
+    if(this === window.JsonLdProcessor.prototype) {
+      return '[object JsonLdProcessorPrototype]';
+    } else if(this && this.constructor === window.JsonLdProcessor) {
+      return '[object JsonLdProcessor]';
+    }
+    return toString.apply(this, arguments);
+  };
+
+  window.processor = new JsonLdProcessor();
+
+  options.readFile('./tests/webidl/JsonLdProcessor.idl').then(idl => {
+    setup({explicit_done: true});
+    var idl_array = new IdlArray();
+    idl_array.add_idls(idl);
+    idl_array.add_objects({JsonLdProcessor: ['window.processor']});
+    idl_array.test();
+    done();
+  }).catch(err => {
+    console.error('WebIDL Error', err);
+    reject(err);
   });
 });
 
