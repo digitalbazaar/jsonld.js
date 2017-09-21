8870e4ae6b5818cb4e6f03fadcc66b3651b1f2e9
David I. Lehn <dlehn@digitalbazaar.com>
2017-09-21 00:21:49 -0400

    More toRDF test updates.

--- tests/misc.js
+++ tests/misc.js
@@ -26,7 +26,6 @@ describe('other toRDF tests', () => {
   it('should process with options and promise', done => {
     const p = jsonld.toRDF({}, {});
     assert(p instanceof Promise);
-    // catch and fail first, then check output
     p.catch(e => {
       assert.fail();
     }).then(output => {
@@ -38,7 +37,6 @@ describe('other toRDF tests', () => {
   it('should process with no options and promise', done => {
     const p = jsonld.toRDF({});
     assert(p instanceof Promise);
-    // catch and fail first, then check output
     p.catch(e => {
       assert.fail();
     }).then(output => {
@@ -54,10 +52,9 @@ describe('other toRDF tests', () => {
     });
   });
 
-  it.skip('should fail with no args and promise', done => {
+  it('should fail with no args and promise', done => {
     const p = jsonld.toRDF();
     assert(p instanceof Promise);
-    // fail first if error not thrown, then check error
     p.then(output => {
       assert.fail();
     }).catch(e => {
@@ -66,13 +63,22 @@ describe('other toRDF tests', () => {
     })
   });
 
-  it('should fail for bad format', done => {
-    const doc = {
-      '@id': 'https://exmaple.com/',
-      'https://example.com/test': 'test'
-    };
-    jsonld.toRDF(doc, {format: 'bogus'}, (err, output) => {
+  it('should fail for bad format and callback', done => {
+    jsonld.toRDF({}, {format: 'bogus'}, (err, output) => {
       assert(err);
+      assert.equal(err.name, 'jsonld.UnknownFormat');
+      done();
+    });
+  });
+
+  it('should fail for bad format and promise', done => {
+    const p = jsonld.toRDF({}, {format: 'bogus'});
+    assert(p instanceof Promise);
+    p.then(() => {
+      assert.fail();
+    }).catch(e => {
+      assert(e);
+      assert.equal(e.name, 'jsonld.UnknownFormat');
       done();
     });
   });
@@ -87,62 +93,6 @@ describe('other toRDF tests', () => {
       assert.equal(
         output,
         '<https://example.com/> <https://example.com/test> "test" .\n');
-      done();
-    });
-  });
-
-  it('should handle default dataset format', done => {
-    const doc = {
-      '@id': 'https://exmaple.com/',
-      'https://example.com/test': 'test'
-    };
-    jsonld.toRDF(doc, {}, (err, output) => {
-      assert.ifError(err);
-      assert.deepEqual(output, {
-        "@default": [{
-          "subject": {
-            "type": "IRI",
-            "value": "https://exmaple.com/"
-          },
-          "predicate": {
-            "type": "IRI",
-            "value": "https://example.com/test"
-          },
-          "object": {
-            "type": "literal",
-            "value": "test",
-            "datatype": "http://www.w3.org/2001/XMLSchema#string"
-          }
-        }]
-      });
-      done();
-    });
-  });
-
-  it.skip('should handle no options', done => {
-    const doc = {
-      '@id': 'https://exmaple.com/',
-      'https://example.com/test': 'test'
-    };
-    jsonld.toRDF(doc, (err, output) => {
-      assert.ifError(err);
-      assert.deepEqual(output, {
-        "@default": [{
-          "subject": {
-            "type": "IRI",
-            "value": "https://exmaple.com/"
-          },
-          "predicate": {
-            "type": "IRI",
-            "value": "https://example.com/test"
-          },
-          "object": {
-            "type": "literal",
-            "value": "test",
-            "datatype": "http://www.w3.org/2001/XMLSchema#string"
-          }
-        }]
-      });
       done();
     });
   });
