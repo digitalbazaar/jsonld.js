7fcbdcd088159806b19b5fecdf3d5d7368a1b28f
Dave Longley <dlongley@digitalbazaar.com>
2017-10-11 12:49:08 -0400

    Allow empty lists to be compacted to any @list container term.

--- tests/test-common.js
+++ tests/test-common.js
@@ -29,7 +29,7 @@ const manifest = options.manifest || {
 const TEST_TYPES = {
   'jld:CompactTest': {
     skip: {
-      regex: [/#t0073/, /#t0074/],
+      regex: [/#t0073/],
       specVersion: ['json-ld-1.1']
     },
     fn: 'compact',
