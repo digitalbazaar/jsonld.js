7c2b31251707f72c59d84f5d62c1be7c8b8b92e6
David I. Lehn <dlehn@digitalbazaar.com>
2017-10-13 18:34:43 -0400

    Fix karma constant rebuild issue.

--- karma.conf.js
+++ karma.conf.js
@@ -43,7 +43,10 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'tests/test-karma.js'
+      {
+        pattern: 'tests/test-karma.js',
+        watched: false, served: true, included: true
+      }
     ],
 
     // list of files to exclude
