073fc330b8c1e89a1bfc358cfa6616bc8d2edfb5
David I. Lehn <dlehn@digitalbazaar.com>
2019-02-01 20:09:58 -0500

    Switch to eslint.

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "commonjs": true,
+    "es6": true,
+    "node": true
+  },
+  "extends": "eslint-config-digitalbazaar",
+  "parserOptions": {
+    "ecmaVersion": 2018
+  },
+  "rules": {
+    "quotes": [
+      "error",
+      "single"
+    ]
+  }
+};
