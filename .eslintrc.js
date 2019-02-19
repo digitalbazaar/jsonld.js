b8249ec54bce2bd5e1053d0e8768f255b1cdb8c2
David I. Lehn <dlehn@digitalbazaar.com>
2019-02-18 22:43:46 -0500

    Update eslint config.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,9 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "commonjs": true,
-    "es6": true,
-    "node": true
+  env: {
+    browser: true,
+    commonjs: true,
+    node: true
   },
-  "extends": "eslint-config-digitalbazaar",
-  "parserOptions": {
-    "ecmaVersion": 2018
-  },
-  "rules": {
-    "quotes": [
-      "error",
-      "single"
-    ]
-  }
+  extends: 'eslint-config-digitalbazaar',
+  root: true
 };
