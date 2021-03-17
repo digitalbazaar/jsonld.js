308e5c6b290b5caa4d770868dd4643ef2328e542
David I. Lehn <dlehn@digitalbazaar.com>
2021-03-17 01:09:01 -0400

    Cleanup.

--- karma.conf.js
+++ karma.conf.js
@@ -50,8 +50,7 @@ module.exports = function(config) {
     ],
 
     // list of files to exclude
-    exclude: [
-    ],
+    exclude: [],
 
     // preprocess matching files before serving them to the browser
     // available preprocessors:
