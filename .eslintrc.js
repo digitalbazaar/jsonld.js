2eadae70d32056cb9d3c5a35c87e239dc36f4b59
David I. Lehn <dlehn@digitalbazaar.com>
2021-04-06 21:57:12 -0400

    Use globalThis and split out platform support.
    - Use `globalThis` to set globals in browser contexts.
    - Split platform support out into Node.js and browser files.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
   env: {
     browser: true,
     commonjs: true,
-    node: true
+    node: true,
+    es2020: true
   },
   extends: 'eslint-config-digitalbazaar',
   root: true
